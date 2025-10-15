a3c21d96027030ea9cea7cb72e5278a6576600ee
TimoH447 <timo.hauser66@gmail.com>
2025-10-15 22:07:39 +0200

    added prototype dialog box updates to totem scene for dialogs

--- totem.gd
+++ totem.gd
@@ -1,29 +1,42 @@
-extends Area2D
+extends Node2D
 
 var player_in_range = false
 var player
+var active_dialog = false
 @export var mask: Mask
+@export var dialog_key = ""
 
+@onready var interaction_area = $InteractionArea
+@onready var proximity_area = $ProximityArea
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	interaction_area.body_entered.connect(_on_interaction_enter)
+	interaction_area.body_exited.connect(_on_interaction_exit)
+	proximity_area.body_exited.connect(_on_proximity_exit)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if player_in_range and Input.is_action_just_pressed("interact"):
+func _input(event: InputEvent) -> void:
+	if player_in_range and event.is_action_pressed("interact") and !active_dialog:
 		if player.is_on_floor():
 			if mask:
+				active_dialog = true
 				player.totem_interaction(mask)
+				SignalBus.emit_signal("display_dialog", dialog_key)
+	elif active_dialog and event.is_action_pressed("interact"):
+		SignalBus.emit_signal("hide_dialog")
+		active_dialog = false
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_interaction_enter(body: Node2D) -> void:
 	if body.name == "Player":
 		player = body
 		player_in_range = true
 
-
-func _on_body_exited(body: Node2D) -> void:
+func _on_interaction_exit(body: Node2D) -> void:
 	if body.name == "Player":
 		player_in_range = false
+
+func _on_proximity_exit(body):
+	print("player ot our range")
+	if active_dialog:
+		active_dialog = false
+		SignalBus.emit_signal("hide_dialog")
