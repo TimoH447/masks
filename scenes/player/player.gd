03e31aef98dd209d57eebaaa174c438a28091d72
TimoH447 <timo.hauser66@gmail.com>
2025-10-17 21:30:50 +0200

    cave layout start

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -27,9 +27,16 @@ var mask_inventory = {
 	"air_mask": null
 }
 
+@onready var animation = $StandardAnimation
+
+func _ready() -> void:
+	animation.visible = true
+
 func _physics_process(delta: float) -> void:
+	get_animation()
 	if velocity.y >0:
 		is_falling = true
+		animation.animation = "fall"
 	if velocity.y>1000:
 		was_high_fall=true
 		
@@ -37,7 +44,6 @@ func _physics_process(delta: float) -> void:
 		# Handle jump.
 		is_jumping = false
 		if was_high_fall:
-			print("highfall")
 			start_stun()
 		last_direction = 0
 	# Jump behaviour and gravity
@@ -53,12 +59,14 @@ func _physics_process(delta: float) -> void:
 			# Bouncing of walls
 			if velocity.x == 0:
 				last_direction *= -0.5
+				animation.flip_h = last_direction > 0
 			velocity.x = last_direction*SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED*0.02)
 			
 	# Jump logic
 	if Input.is_action_pressed("ui_accept") and can_channel_jump() and !is_stunned:
+		animation.animation = "channel"
 		channeling = true
 		jump_charge_time += delta
 	if channeling and (jump_charge_time > 1 or Input.is_action_just_released("ui_accept")) and !is_stunned and !is_jump_cooldown:
@@ -74,12 +82,24 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED* SLIPPERINESS)
 	move_and_slide()
 
+func get_animation():
+	var animation_key = ""
+	if !is_stunned and !channeling:
+		animation_key = "default"
+		if is_on_floor() and velocity.x != 0:
+			#animation= "run"
+			animation.flip_h = velocity.x >0
+		elif !is_on_floor():
+			animation_key = "jump_up"
+	animation.animation = animation_key
+
 func can_channel_jump():
 	if is_wall_jumping:
 		return true
 	return !is_stunned and is_on_floor() and has_jump_ability
 	
 func start_jump():
+	animation.animation = "jump_up"
 	start_jump_cd()
 	last_direction = 0
 	if Input.is_action_just_pressed("move_right") or Input.is_action_pressed("move_right"):
@@ -95,6 +115,7 @@ func start_jump():
 		is_wall_jumping = false
 	velocity.x = last_direction * SPEED
 	
+	animation.flip_h = last_direction > 0
 	jump_charge_time = 0
 
 func start_jump_cd():
@@ -103,20 +124,30 @@ func start_jump_cd():
 	is_jump_cooldown = false
 	
 func start_stun():
+	animation.play("stun")
 	was_high_fall = false
 	is_stunned = true
 	await get_tree().create_timer(2).timeout
 	is_stunned = false
+	animation.play("default")
 
 func totem_interaction(mask):
 	mask_inventory[mask.get_mask_name()]= mask
 	put_on_mask(mask)
 
 func put_on_mask(mask):
+	animation.visible = false
 	if current_mask:
 		current_mask.remove(self)
 	current_mask = mask
 	mask.apply(self)
+	if mask.get_mask_name()=="fire_mask":
+		animation = $FireAnimation
+	elif mask.get_mask_name()=="air_mask":
+		animation = $AirAnimation
+	elif mask.get_mask_name()=="forrest_mask":
+		animation = $ForrestAnimation
+	animation.visible = true
 	
 func has_all_masks():
 	return mask_inventory["fire_mask"] and mask_inventory["air_mask"] and mask_inventory["forrest_mask"]
@@ -126,6 +157,8 @@ func remove_masks_from_inventory():
 	mask_inventory["air_mask"] = null
 	mask_inventory["forrest_mask"] = null
 	current_mask.remove(self)
+	animation = $StandardAnimation
+	animation.visible = true
 	current_mask = null
 	
 	
